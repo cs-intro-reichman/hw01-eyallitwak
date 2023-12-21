e8fa6c809e7a7822ed5feee24ba62927670faad8
Eyal Litwak <eyal.litwak@post.runi.ac.il>
2023-12-21 17:19:26 +0200

    second commit (Gen3/GenThree fix)

--- GenThree.java
+++ GenThree.java
@@ -4,6 +4,23 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+		//gets lower and upper limit from user
+		int lowerLimit = Integer.parseInt(args[0]),
+			upperLimit = Integer.parseInt(args[1]);
+		
+		//declares possible range of generated random numbers
+		int range = upperLimit - lowerLimit;
+		
+		int rand1 = lowerLimit + (int) (Math.random() * range),
+			rand2 = lowerLimit + (int) (Math.random() * range),
+			rand3 = lowerLimit + (int) (Math.random() * range);
+		
+		System.out.println(rand1);
+		System.out.println(rand2);
+		System.out.println(rand3);
+		
+		//checks which random number is minimal
+		int minimalRand = Math.min(Math.min(rand1, rand2), rand3);
+		System.out.println("The minimal generated number was " + minimalRand);
 	}
 }
